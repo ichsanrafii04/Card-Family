7932a49f3e02842a7f212e975d8151ac44d05d89
ichsanrafii04 <143162678+ichsanrafii04@users.noreply.github.com>
2023-11-03 21:23:01 +0800

    Add files via upload
    upload

---
+++ Assets/Java.js
@@ -0,0 +1,47 @@
+const container = document.getElementById("container");
+
+const Ayah = document.getElementById("portoayah");
+const Bunda = document.getElementById("portobunda");
+const Ala = document.getElementById("portoala");
+const Rafi = document.getElementById("portorafi");
+const Dio = document.getElementById("portodio");
+
+function AyahBtn() {
+  Ayah.style.display = "block";
+  if ((Ayah.style.display = "block")) {
+    container.style.display = "none";
+  }
+}
+function BundaBtn() {
+  Bunda.style.display = "block";
+  if ((Bunda.style.display = "block")) {
+    container.style.display = "none";
+  }
+}
+function AlaBtn() {
+  Ala.style.display = "block";
+  if ((Ala.style.display = "block")) {
+    container.style.display = "none";
+  }
+}
+function RafiBtn() {
+  Rafi.style.display = "block";
+  if ((Rafi.style.display = "block")) {
+    container.style.display = "none";
+  }
+}
+function DioBtn() {
+  Dio.style.display = "block";
+  if ((Dio.style.display = "block")) {
+    container.style.display = "none";
+  }
+}
+
+// const screen = document.getElementsByClassName("undermaintenance");
+
+// function tes() {
+//   screen.style.display = "block";
+//   if ((screen.style.display = "block")) {
+//     Rafi.style.display = "none"
+//   }
+// }
